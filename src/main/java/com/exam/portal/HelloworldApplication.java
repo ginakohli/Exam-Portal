008751585c3e6bad52111ceed23fd313d92b9d8f
Gina_Kohli <kohli.gina@gmail.com>
2022-06-06 12:25:52 +0530

    E-portal

---
+++ src/main/java/com/exam/portal/HelloworldApplication.java
@@ -0,0 +1,15 @@
+package com.exam.portal;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloworldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloworldApplication.class, args);
+	}
+
+}
